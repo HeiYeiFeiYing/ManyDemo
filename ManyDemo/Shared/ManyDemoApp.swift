abe592013ba6a4b50f62b32972851e7c1a4e1510
HeiYeiFeiYing <1273434710@qq.com>
2021-03-08 11:13:09 +0800

    Add files via upload
    暂未配置,当前空项目

---
+++ ManyDemo/Shared/ManyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManyDemoApp.swift
+//  Shared
+//
+//  Created by RT on 2021/3/8.
+//
+
+import SwiftUI
+
+@main
+struct ManyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
